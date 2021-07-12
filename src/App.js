ee4898a3ea73f6cc6ce9f34580d9bbd8f2f8d821
emileypalmquist <emiley.palmquist@icloud.com>
2021-07-12 14:37:17 -0600

    added comments component with one comment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Comments from "./Comments";
 
 function App() {
   return (
     <div className="App">
       <h1>APP</h1>
       <p>This is cool</p>
+      <Comments />
     </div>
   );
 }
---
+++ src/Comments.js
@@ -0,0 +1,12 @@
+function Comments() {
+  return (
+    <section>
+      <h2>Comments</h2>
+      <ul>
+        <li>ONe comment</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Comments;
